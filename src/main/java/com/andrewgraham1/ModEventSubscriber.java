2c0d4acabefb38f9b91042cfeb94616261470cff
Andrew Graham <andrewgraham1@hotmail.co.uk>
2020-05-28 12:18:26 +0100

    Item Block set-up

--- src/main/java/com/andrewgraham1/ModEventSubscriber.java
+++ src/main/java/com/andrewgraham1/ModEventSubscriber.java
@@ -1,17 +1,50 @@
 package com.andrewgraham1;
 
 import com.andrewgraham1.blade.init.ModItemGroups;
+import com.andrewgraham1.init.ModBlocks;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundCategory;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.IForgeRegistry;
 import net.minecraftforge.registries.IForgeRegistryEntry;
 import net.minecraftforge.registries.ObjectHolder;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 
 @Mod.EventBusSubscriber(modid = SwordMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModEventSubscriber {
+
+    private static final Logger LOGGER = LogManager.getLogger(SwordMod.MODID + " Mod Event Subscriber");
+
+    @SubscribeEvent
+    public static void onRegisterBlocks(RegistryEvent.Register<Item> event){
+        final IForgeRegistry<Item> registry = event.getRegistry();
+        // Automatically register BlockItems for all our Blocks
+        ModBlocks.BLOCKS.getEntries().stream()
+                .map(RegistryObject::get)
+                // You can do extra filtering here if you don't want some blocks to have an BlockItem automatically registered for them
+                // .filter(block -> needsItemBlock(block))
+                // Register the BlockItem for the block
+                .forEach(block -> {
+                    // Make the properties, and make it so that the item will be on our ItemGroup (CreativeTab)
+                    final Item.Properties properties = new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP);
+                    // Create the new BlockItem with the block and it's properties
+                    final BlockItem blockItem = new BlockItem(block, properties);
+                    // Set the new BlockItem's registry name to the block's registry name
+                    blockItem.setRegistryName(block.getRegistryName());
+                    // Register the BlockItem
+                    registry.register(blockItem);
+                });
+        LOGGER.debug("Registered BlockItems");
+    }
 
     @SubscribeEvent
     public static void onRegisterItems(RegistryEvent.Register<Item> event) {
